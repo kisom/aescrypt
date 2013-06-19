937981dcf8b7a541ecfbf15e62c51d68501fdb29
Kyle Isom <coder@kyleisom.net>
2013-06-19 15:29:38 -0600

    Add package doc.

---
+++ doc.go
@@ -0,0 +1,7 @@
+CryptoKit is a set of packages that provide core functions for
+safely employing cryptography, while using FIPS ciphers as the
+underlying cryptographic primitives. It is heavily inspired by
+[NaCL](http://nacl.cace-project.eu/) The aim is provide developers with
+a well-documented, properly-written, FIPS-compliant set of tools for
+proper encryption. CryptoKit is both the name of the suite and the name
+of module that is expexpected to be relevant to most developers.
