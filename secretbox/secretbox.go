339ffd067ec595c422a02902b36d9c64bca12886
Kyle Isom <coder@kyleisom.net>
2013-06-18 23:39:29 -0600

    Initial commit.

---
+++ secretbox/secretbox.go
@@ -0,0 +1,158 @@
+/*
+	secretbox is used to authenticate and secure small messages. It
+	provides an interface similar to NaCL, but uses AES-128 in CTR
+	mode with HMAC-SHA-256 for securing messages.
+
+	Messages should be secured using the Seal function, and recovered
+	using the Open function. A box (or authenticated and encrypted
+	message) will be Overhead bytes longer than the message it
+	came from; this package will not obscure the length of the
+	message. Keys, if they are not generated using the GenerateKey
+	function, should be KeySize bytes long. The KeyIsSuitable function
+	may be used to test a key is the proper length.
+
+	The boxes used in this package are suitable for 20-year security.
+*/
+package secretbox
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/subtle"
+	"fmt"
+	"io"
+)
+
+const cryptKeySize = 16
+const tagKeySize = 32
+
+// KeySize is the number of bytes a valid key should be.
+const KeySize = cryptKeySize + tagKeySize
+
+// Overhead is the number of bytes of overhead when boxing a message.
+const Overhead = aes.BlockSize + sha256.Size
+
+var (
+	errinvalidKeySize    = fmt.Errorf("invalid key size")
+	errinvalidCiphertext = fmt.Errorf("invalid ciphertext")
+)
+
+// The default source for random data is the crypto/rand package's Reader.
+var PRNG = rand.Reader
+
+type Key []byte
+type nonce []byte
+
+func GenerateKey() (Key, error) {
+	var key Key = make([]byte, KeySize)
+
+	_, err := io.ReadFull(PRNG, key)
+	return key, err
+}
+
+func generateNonce() (nonce, error) {
+	var n nonce = make([]byte, aes.BlockSize)
+
+	_, err := io.ReadFull(PRNG, n)
+	return n, err
+
+}
+
+func encrypt(key []byte, in []byte) (out []byte, err error) {
+	var iv nonce
+	if iv, err = generateNonce(); err != nil {
+		return
+	}
+
+	out = make([]byte, len(in)+aes.BlockSize)
+	for i := 0; i < aes.BlockSize; i++ {
+		out[i] = iv[i]
+		iv[i] = 0
+	}
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+ctr := cipher.NewCTR(c, out[:aes.BlockSize])
+	ctr.XORKeyStream(out[aes.BlockSize:], in)
+	return
+}
+
+func computeTag(key []byte, in []byte) (tag []byte) {
+	h := hmac.New(sha256.New, key)
+	h.Write(in)
+	return h.Sum(nil)
+}
+
+func checkTag(key, in []byte) bool {
+	ctlen := len(in) - sha256.Size
+	tag := in[ctlen:]
+	ct := in[:ctlen]
+	actualTag := computeTag(key, ct)
+	return subtle.ConstantTimeCompare(tag, actualTag) == 1
+}
+
+func decrypt(key []byte, in []byte) (out []byte, err error) {
+	if len(in) < aes.BlockSize {
+		return nil, errinvalidCiphertext
+	}
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+
+	iv := in[:aes.BlockSize]
+	ct := in[aes.BlockSize:]
+	ctr := cipher.NewCTR(c, iv)
+	out = make([]byte, len(ct))
+	ctr.XORKeyStream(out, ct)
+	return
+}
+
+// Seal returns an authenticated and encrypted message, and a boolean
+// indicating whether the sealing operation was successful. If it returns
+//true, the message was successfully sealed. The box will be Overhead
+// bytes longer than the message.
+func Seal(message []byte, key Key) (box []byte, ok bool) {
+	if !KeyIsSuitable(key) {
+		return
+	}
+
+	ct, err := encrypt(key[:cryptKeySize], message)
+	if err != nil {
+		return
+	}
+	tag := computeTag(key[cryptKeySize:], ct)
+	box = append(ct, tag...)
+	ok = true
+	return
+}
+
+// Open authenticates and decrypts a sealed message, also returning
+// whether the message was successfully opened. If this is false, the
+// message must be discarded. The returned message will be Overhead
+// bytes shorter than the box.
+func Open(box []byte, key Key) (message []byte, ok bool) {
+	if !KeyIsSuitable(key) {
+		return
+	} else if len(box) <= Overhead {
+		return
+	}
+
+	msgLen := len(box) - sha256.Size
+	message, err := decrypt(key[:cryptKeySize], box[:msgLen])
+	ok = checkTag(key[cryptKeySize:], box)
+	ok = ok && err == nil
+	return
+}
+
+// IsKeySuitable returns true if the byte slice represents a valid
+// secretbox key.
+func KeyIsSuitable(key []byte) bool {
+	return subtle.ConstantTimeEq(int32(len(key)), int32(KeySize)) == 1
+}
---
+++ strongbox/strongbox_test.go
@@ -0,0 +1,125 @@
+package strongbox
+
+import "bytes"
+import "fmt"
+import "io/ioutil"
+import "testing"
+
+var testMessages = []string{
+	"Hello, world.",
+	"Yes... yes. This is a fertile land, and we will thrive. We will rule over all this land, and we will call it... This Land.",
+	"Ah! Curse your sudden but inevitable betrayal!",
+	"And I'm thinkin' you weren't burdened with an overabundance of schooling. So why don't we just ignore each other until we go away?",
+	"Sir, I think you have a problem with your brain being missing.",
+	"It's the way of life in my findings that journeys end when and where they want to; and that's where you make your home.",
+	"I get confused. I remember everything. I remember too much. And... some of it's made up, and... some of it can't be quantified, and... there's secrets... and...",
+	"Yeah, we're pretty much just giving each other significant glances and laughing incessantly.",
+	"Jayne, go play with your rainstick.",
+}
+
+var (
+	testBoxes   = make([]string, len(testMessages))
+	testBoxFile []byte
+	testGoodKey Key
+	testBadKey  Key
+)
+
+func TestKeyGeneration(t *testing.T) {
+	var err error
+	testGoodKey, err = GenerateKey()
+	if err != nil {
+		fmt.Println("Failed to generate key:", err.Error())
+		t.FailNow()
+	}
+	testBadKey, err = GenerateKey()
+	if err != nil {
+		fmt.Println("Failed to generate key:", err.Error())
+		t.FailNow()
+	}
+}
+
+func TestBoxing(t *testing.T) {
+	for i := 0; i < len(testMessages); i++ {
+		box, ok := Seal([]byte(testMessages[i]), testGoodKey)
+		if !ok {
+			fmt.Println("Boxing failed: message", i)
+			t.FailNow()
+		}
+		testBoxes[i] = string(box)
+	}
+}
+
+func TestUnboxing(t *testing.T) {
+	for i := 0; i < len(testMessages); i++ {
+		message, ok := Open([]byte(testBoxes[i]), testGoodKey)
+		if !ok {
+			fmt.Println("Unboxing failed: message", i)
+			t.FailNow()
+		} else if string(message) != testMessages[i] {
+			fmt.Printf("Unboxing failed: expected '%s', got '%s'\n",
+				testMessages[i], string(message))
+			t.FailNow()
+		}
+	}
+}
+
+func TestUnboxingFails(t *testing.T) {
+	for i := 0; i < len(testMessages); i++ {
+		_, ok := Open([]byte(testBoxes[i]), testBadKey)
+		if ok {
+			fmt.Println("Unboxing should have failed with bad key:", i)
+			t.FailNow()
+		}
+	}
+}
+
+func TestLargerBox(t *testing.T) {
+	var err error
+	testBoxFile, err = ioutil.ReadFile("testdata/TEST.txt")
+	if err != nil {
+		fmt.Println("Failed to read test data:", err.Error())
+		t.FailNow()
+	}
+
+	box, ok := Seal(testBoxFile, testGoodKey)
+	if !ok {
+		fmt.Println("Failed to box message.")
+		t.FailNow()
+	}
+
+	message, ok := Open(box, testGoodKey)
+	if !ok {
+		fmt.Println("Failed to unbox message.")
+		t.FailNow()
+	}
+
+	if !bytes.Equal(message, testBoxFile) {
+		fmt.Println("Recovered message is invalid.")
+		t.FailNow()
+	}
+}
+
+func BenchmarkSeal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, ok := Seal(testBoxFile, testGoodKey)
+		if !ok {
+			fmt.Println("Couldn't seal message: benchmark aborted.")
+			b.FailNow()
+		}
+	}
+}
+
+func BenchmarkOpen(b *testing.B) {
+	box, ok := Seal(testBoxFile, testGoodKey)
+	if !ok {
+		fmt.Println("Can't seal message: benchmark aborted.")
+		b.FailNow()
+	}
+	for i := 0; i < b.N; i++ {
+		_, ok := Open(box, testGoodKey)
+		if !ok {
+			fmt.Println("Couldn't open message: benchmark aborted.")
+			b.FailNow()
+		}
+	}
+}
