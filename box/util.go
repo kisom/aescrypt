6e5e6dcb017d6fcdcca207907439159de3329422
Kyle <kyle@tyrfingr.is>
2013-11-11 15:23:24 -0700

    Remove stray debug printf.

--- box/util.go
+++ box/util.go
@@ -3,7 +3,6 @@ package box
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
 	"math/big"
 )
 
@@ -85,7 +84,6 @@ func (b *br) Next() []byte {
 	var dlen uint32
 	b.err = binary.Read(b.buf, binary.BigEndian, &dlen)
 	if b.err == nil {
-		fmt.Printf("dlen: %d\t\tbuflen: %d\n", int(dlen), b.buf.Len())
 		if int(dlen) > b.buf.Len() {
 			return nil
 		} else if int(dlen) <= 0 {
